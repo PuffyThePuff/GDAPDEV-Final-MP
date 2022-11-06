df1e302165da6fda3c8071828f9971df2a42c1d9
Nathan-Macabanti <73865062+Nathan-Macabanti@users.noreply.github.com>
2022-11-06 17:39:00 +0800

    Update Scroller.cs

--- Assets/Scripts/AnimationsThroughScript/Scroller.cs
+++ Assets/Scripts/AnimationsThroughScript/Scroller.cs
@@ -8,7 +8,9 @@ public class Scroller : MonoBehaviour
     [SerializeField] private RawImage _img;
     [SerializeField] private float _x, _y;
 
-    private Vector2 scrollSpeed = new Vector2();
+    //private Vector2 scrollSpeed = new Vector2();
+    //private Vector2 position = new Vector2();
+    private Rect scrollRect = new Rect();
 
     private void Start()
     {
@@ -17,8 +19,14 @@ public class Scroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scrollSpeed.x = _x;
-        scrollSpeed.y = _y;
-        _img.uvRect = new Rect(_img.uvRect.position + scrollSpeed * Time.deltaTime, _img.uvRect.size);
+        //scrollSpeed.x = _x;
+        //scrollSpeed.y = _y;
+        float posX = _img.uvRect.position.x + _x * Time.deltaTime;
+        float posY = _img.uvRect.position.y + _y * Time.deltaTime;
+        scrollRect.x = posX;
+        scrollRect.y = posY;
+        scrollRect.width = _img.uvRect.width;
+        scrollRect.height = _img.uvRect.height;
+        _img.uvRect = scrollRect;
     }
 }
