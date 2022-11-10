bfcdb2b54131b7198f383860288b3bdbb033feba
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2022-11-10 15:34:42 +0800

    Added Ads manager and package

---
+++ Assets/Scripts/Advertisement/AdsManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour, IUnityAdsInitializationListener
+{
+    [SerializeField] private string _androidGameID;
+    [SerializeField] private string _iOSGameID;
+    [SerializeField] private bool _testMode = true;
+
+    private string _gameID;
+
+    #region singleton code
+    //Miguel's really cool singleton code he made in 2020 and probably still works
+    //put Singleton = this in Awake()
+    private static AdsManager _singleton;
+
+    //getter and setter for singleton
+    public static AdsManager Singleton
+    {
+        get => _singleton;
+
+        private set
+        {
+            if (_singleton == null)
+            {
+                _singleton = value;
+            }
+            else if (_singleton != value)
+            {
+                Debug.Log($"{nameof(AdsManager)} instance already exists, destroying duplicate");
+                Destroy(value);
+            }
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        Singleton = this;
+        InitializeAds();
+        DontDestroyOnLoad(this);
+    }
+
+    private void InitializeAds()
+    {
+        _gameID = (Application.platform == RuntimePlatform.IPhonePlayer) ? _iOSGameID : _androidGameID;
+
+        Advertisement.Initialize(_gameID, _testMode, this);
+        Debug.Log("initializing ads system: " + _gameID);
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads initialization failed: {error.ToString()} - {message}");
+    }
+}
